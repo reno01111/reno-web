3a5e2a433938edeadd7b865aa6e33df71420ec8b
reno01111 <reno.itigo@gmail.com>
2024-09-01 23:03:14 +0900

    ポリシー本文追加

--- pages/docs/privacy.tsx
+++ pages/docs/privacy.tsx
@@ -12,7 +12,35 @@ export function Privacy() {
         pageImgWidth={1280}
         pageImgHeight={960}
       />
-      <h1 className="text-3xl font-semibold">Privacy Policy</h1>
+      <div className="container m-auto h-screen w-100 flex items-center justify-center">
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-semibold text-center m-10">
+            Privacy Policy
+          </h1>
+          <div className="m-2 content mx-auto my-6 flex w-11/12 flex-col gap-y-2 sm:w-4/5 lg:w-2/3">
+            <p>
+              このサイトでは利用状況を分析するために Google アナリティクス
+              を使用しています。Google アナリティクス は Cookie
+              を使用して当サイトへのアクセス情報を Google に送信しています。
+            </p>
+            <p>
+              取得したデータは本サイトの改善に役立てることのみに使用するとし、個人の特定やその他個人の不利益に関わることはいたしません。
+              規約の詳細に関しましては、
+              <Link href="https://marketingplatform.google.com/about/analytics/terms/jp/">
+                「Googleアナリティクス利用規約」
+              </Link>
+              をご覧ください。
+            </p>
+            <p>
+              Google アナリティクス でデータが収集、処理される仕組みについては
+              <Link href="https://policies.google.com/technologies/partner-sites">
+                「Googleのサービスを使用するサイトやアプリから収集した情報のGoogleによる使用」
+              </Link>
+              をご覧ください。
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
