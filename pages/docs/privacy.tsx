bbaaad5bbc2e0050a06482d5fbdab04c43aee7ea
reno01111 <reno.itigo@gmail.com>
2024-09-01 23:47:30 +0900

    ポリシー本文追加

--- pages/docs/privacy.tsx
+++ pages/docs/privacy.tsx
@@ -12,38 +12,76 @@ export function Privacy() {
         pageImgWidth={1280}
         pageImgHeight={960}
       />
-      <div className="container m-auto h-screen w-100 flex items-center justify-center">
+      <div className="container m-auto w-100 flex items-center justify-center">
         <div className="flex flex-col">
           <h1 className="text-3xl font-semibold text-center m-10">
             Privacy Policy
+            <small className="text-sm opacity-80">プライバシーポリシー</small>
           </h1>
           <div className="m-2 content mx-auto my-6 flex w-11/12 flex-col gap-y-2 sm:w-4/5 lg:w-2/3">
-            <p>
-              このサイトでは利用状況を分析するために Google アナリティクス
-              を使用しています。Google アナリティクス は Cookie
-              を使用して当サイトへのアクセス情報を Google に送信しています。
-            </p>
-            <p>
-              取得したデータは本サイトの改善に役立てることのみに使用するとし、個人の特定やその他個人の不利益に関わることはいたしません。
-              規約の詳細に関しましては、
-              <Link
-                href="https://marketingplatform.google.com/about/analytics/terms/jp/"
-                className="text-blue-500 underline"
-              >
-                「Googleアナリティクス利用規約」
-              </Link>
-              をご覧ください。
-            </p>
-            <p>
-              Google アナリティクス でデータが収集、処理される仕組みについては
-              <Link
-                href="https://policies.google.com/technologies/partner-sites"
-                className="text-blue-500 underline"
-              >
-                「Googleのサービスを使用するサイトやアプリから収集した情報のGoogleによる使用」
-              </Link>
-              をご覧ください。
-            </p>
+            <div className="mb-5">
+              <h1 className="text-lg font-bold">はじめに</h1>
+              <p>
+                はじめに
+                この利用規約は、Reno.（以下、「当サイト」といいます。）が公開するサイトの利用条件を定めるものです。
+                <br />
+                ユーザーの皆様は本利用規約に同意の上、本サービスをご利用ください。
+              </p>
+            </div>
+            <div className="mb-5">
+              <h1 className="text-lg font-bold">個人情報の利用目的</h1>
+              <p>
+                当ブログでは、お問い合わせの際、名前やメールアドレス等の個人情報を入力いただく場合がございます。
+                取得した個人情報は、お問い合わせに対する回答や必要な情報を電子メールなどでご連絡する場合に利用させていただくものであり、これらの目的以外では利用いたしません。
+              </p>
+            </div>
+            <div className="mb-5">
+              <h1 className="text-lg font-bold">広告について</h1>
+              <p>
+                当ブログでは、第三者配信の広告サービス（Googleアドセンス）を利用しており、ユーザーの興味に応じた商品やサービスの広告を表示するため、クッキー（Cookie）を使用しております。
+                <br />
+                クッキーを使用することで当サイトはお客様のコンピュータを識別できるようになりますが、お客様個人を特定できるものではありません。
+              </p>
+              <br />
+              <p>
+                Cookieを無効にする方法やGoogleアドセンスに関する詳細は
+                <Link
+                  href="https://policies.google.com/technologies/ads?gl=jp"
+                  className="text-blue-500 underline"
+                >
+                  「広告 – ポリシーと規約 – Google」
+                </Link>
+                をご確認ください。
+              </p>
+            </div>
+            <div className="mb-5">
+              <h1 className="text-lg font-bold">アクセス解析ツールについて</h1>
+              <p>
+                当ブログでは、Googleによるアクセス解析ツール「Googleアナリティクス」を利用しています。このGoogleアナリティクスはトラフィックデータの収集のためにクッキー（Cookie）を使用しております。
+                <br />
+                トラフィックデータは匿名で収集されており、個人を特定するものではありません。
+              </p>
+            </div>
+            <div className="mt-20 mb-5">
+              <h1 className="text-lg font-bold">免責事項</h1>
+              <p>
+                当ブログからのリンクやバナーなどで移動したサイトで提供される情報、サービス等について一切の責任を負いません。
+                <br />
+                また当ブログのコンテンツ・情報について、できる限り正確な情報を提供するように努めておりますが、正確性や安全性を保証するものではありません。情報が古くなっていることもございます。
+                <br />
+                当サイトに掲載された内容によって生じた損害等の一切の責任を負いかねますのでご了承ください。
+              </p>
+            </div>
+            <div className="mt-20 mb-5">
+              <h1 className="text-lg font-bold">お問い合わせについて</h1>
+              <p>
+                本ポリシー又はサイトに関するお問い合わせは，下記の窓口までお願いいたします。
+                <br />
+                <Link href="/contact" className="text-blue-500 underline">
+                  お問い合わせフォーム
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
