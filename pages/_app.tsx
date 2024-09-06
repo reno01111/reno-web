f0055079807cfab1a7d0bf87e8e451d4b714b590
reno01111 <reno.itigo@gmail.com>
2024-09-07 05:14:38 +0900

    googleアナリティクス実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Noto_Serif_JP } from "next/font/google";
 
 import "../styles/globals.css";
@@ -12,10 +15,31 @@ const notoSerifJP = Noto_Serif_JP({
   subsets: ["latin"],
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (window.gtag) {
+        window.gtag("config", process.env.GA_ID as string, {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className={notoSerifJP.className}>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId={process.env.GA_ID as string} />
     </div>
   );
-}
+};
+
+export default App;
